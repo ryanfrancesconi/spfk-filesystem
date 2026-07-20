1bfc7324656ad8470ea48d8aacddd3fe475f8c9e
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-19 17:11:35 -0700

    Fix source file copyright headers to reference this package
    Headers were pointing at the wrong repo (often the package this one
    was split out of), missing the /ryanfrancesconi/<repo> revision-history
    URL entirely, or missing the header line altogether.

--- Sources/SPFKFileSystem/Tags/FinderTagGroup+HexColor.swift
+++ Sources/SPFKFileSystem/Tags/FinderTagGroup+HexColor.swift
@@ -1,4 +1,4 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-utils
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-filesystem
 
 #if os(macOS)
 
